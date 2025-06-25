87769a72d4f602b3895a673d41d50a7a4022457c
Touriz <juandigo30@gmail.com>
2025-06-25 09:51:24 +0200

    Deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'GameHub',
+    loadComponent: () =>
+      import('./pages/storefront/storefront').then((m) => m.Storefront),
+  },
+  {
+    path: 'library',
+    loadComponent: () =>
+      import('./pages/library/library').then(
+        (m) => m.Library,
+      ),
+  },
+    {
+    path: 'admin',
+    loadComponent: () =>
+      import('./pages/admin/admin').then(
+        (m) => m.Admin,
+      ),
+  },
+    {
+    path: 'user',
+    loadComponent: () =>
+      import('./pages/user/user').then(
+        (m) => m.User,
+      ),
+  },
+]
---
+++ src/app/pages/storefront/storefront.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storefront',
+  imports: [],
+  templateUrl: './storefront.html',
+  styleUrl: './storefront.scss'
+})
+export class Storefront {
+
+}
